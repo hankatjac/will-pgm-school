042653c2fcffc887aef4bbbf4277ead875f397f9
hank <hangryan@gmail.com>
2023-03-01 16:36:48 -0500

    1

--- src/apiPath.jsx
+++ src/apiPath.jsx
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8800/api";
-// export const API_URL = "https://zany-lime-rhinoceros-veil.cyclic.app/api";
+// export const API_URL = "http://localhost:8800/api";
+export const API_URL = "https://zany-lime-rhinoceros-veil.cyclic.app/api";
